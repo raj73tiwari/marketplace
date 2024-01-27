c5a44e1b608efa48e6049adc28105205e1191c7e
Sahastranshu Tiwari <93114426+raj73tiwari@users.noreply.github.com>
2024-01-27 15:29:04 +0530

    changed url pattern of homepage

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -23,6 +23,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('basicapp.urls')),
+    path('', include('basicapp.urls')),
     path('user/', include('user.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
